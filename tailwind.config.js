071ece4d07796073f71e7488c52d8fadbb96caac
Mate <mateobauti.maccarone@hotmail.com>
2024-04-07 17:46:43 -0300

    adding nextui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         gray: {
-          533: '#ABA6A6',
+          533: "#ABA6A6",
         },
-      }
+      },
     },
-    fontFamily: {
-      aileronRegular: ['aileron-regular', 'sans-serif'],
-      aileronBold: ['aileron-bold', 'sans-serif'],
-      plain: ['plain-regular', 'sans-serif'],
-      plainBold: ['plain-bold', 'sans-serif'],
-      plainNormal: ['plain-normal', 'sans-serif'],
-      plainLight: ['plain-light', 'sans-serif'],
-      plainItalic: ['plain-italic', 'sans-serif'],
-      plainRegular: ['plain-regular', 'sans-serif'],	
-    }
   },
-  plugins: [require("daisyui")],
-}
+  fontFamily: {
+    aileronRegular: ["aileron-regular", "sans-serif"],
+    aileronBold: ["aileron-bold", "sans-serif"],
+    plain: ["plain-regular", "sans-serif"],
+    plainBold: ["plain-bold", "sans-serif"],
+    plainNormal: ["plain-normal", "sans-serif"],
+    plainLight: ["plain-light", "sans-serif"],
+    plainItalic: ["plain-italic", "sans-serif"],
+    plainRegular: ["plain-regular", "sans-serif"],
+  },
+  darkMode: "dark",
+  plugins: [nextui()],
+};
